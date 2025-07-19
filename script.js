0841c20309812ea3eb7261049d05dbf83f7b07e5
Djota_ <dariojonasdrx@gmail.com>
2025-07-19 13:45:44 -0400

    Funcional Completo

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // Exibir uma mensagem de "Valor insuficiente" caso o valor seja menor que R$1,00.
 
 class Parquimetro {
-    #saldo
+    #saldo;
     constructor(saldo, tempo){
         this.#saldo = 0;
         this.tempo = "";
@@ -16,15 +16,44 @@ class Parquimetro {
     depositar(){
         //Pegar o valor de input e somar ao valor de #saldo
         const valor = parseFloat(document.getElementById("valor").value);
+        const troco = 0;
         //Condição de avisar sobre valor insuficiente se não bater o valor minimo
         if(valor < 1){
             document.getElementById("deposito").textContent= 'Saldo: R$ Valor insuficiente';
-        }else{
-        this.#saldo = valor;
-        //Exibição de saldo atualizado na pagina e formatação de "." para "," nas casas decimais
-        document.getElementById("deposito").textContent = `Saldo: ${this.#saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
-        //Chamada de metodo para calcular o tempo
-        this.minutos()
+        } else if(valor == 1){
+            this.troco = 0
+            this.#saldo = valor;
+            //Exibição de saldo atualizado na pagina e formatação de "." para "," nas casas decimais
+            document.getElementById("deposito").textContent = `Saldo: ${this.#saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            document.getElementById("troco").textContent = `Troco: ${this.troco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            //Chamada de metodo para calcular o tempo
+            this.minutos()
+        } else if (valor > 1 && valor < 1.75){
+            const preco = 1;
+            this.troco = valor - preco;
+            this.#saldo = preco
+            document.getElementById("deposito").textContent = `Saldo: ${this.#saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            document.getElementById("troco").textContent = `Troco: ${this.troco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            this.minutos();
+        } else if (valor == 1.75){
+            this.troco = 0
+            this.#saldo = valor;
+            document.getElementById("deposito").textContent = `Saldo: ${this.#saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            document.getElementById("troco").textContent = `Troco: ${this.troco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            this.minutos();
+        } else if (valor > 1.75 && valor < 3){
+            const preco = 1.75;
+            this.troco = valor - preco;
+            this.#saldo = preco
+            document.getElementById("deposito").textContent = `Saldo: ${this.#saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            document.getElementById("troco").textContent = `Troco: ${this.troco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            this.minutos();
+        } else if (valor == 3){
+            this.troco = 0
+            this.#saldo = valor;
+            document.getElementById("deposito").textContent = `Saldo: ${this.#saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            document.getElementById("troco").textContent = `Troco: ${this.troco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+            this.minutos();
         }
     }
 
@@ -32,10 +61,19 @@ class Parquimetro {
         //Deixando em branco o campo input
         document.getElementById("valor").value= '';
         //Condições baseada na variavel #saldo
-
+        if (this.#saldo == 1){
+            this.tempo = "30 minutos"
+            document.getElementById("tempo").textContent = `Tempo: ${this.tempo}`
+        } else if (this.#saldo == 1.75){
+            this.tempo = "1 Hora (60 minutos)"
+            document.getElementById("tempo").textContent = `Tempo: ${this.tempo}`
+        } else if (this.#saldo == 3){
+            this.tempo = "2 Horas (120 minutos)"
+            document.getElementById("tempo").textContent = `Tempo: ${this.tempo}`
+        }
   }
 }
 
 // instância global
 const p = new Parquimetro();
-p.depositar()
+p.depositar();
